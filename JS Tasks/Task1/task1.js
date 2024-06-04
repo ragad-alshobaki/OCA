9492e5e4345075f1f93a8e2c99815bba72649f43
rgd97 <rgd.alshobaki@gmail.com>
2024-06-04 16:02:33 +0300

    new tasks done

---
+++ JS Tasks/Task1/task1.js
@@ -0,0 +1,48 @@
+/*1st req*/
+var cash = 1000;
+var liabilities = 500;
+var cfr = cash / liabilities;
+console.log(cfr);
+
+/*2nd req*/
+var revenue = 1000;
+var expenses = 500;
+var n_income = revenue - expenses;
+console.log(n_income);
+
+/*3rd req*/
+var liabilities =1000;
+var equity = 500;
+var assets = liabilities + equity;
+console.log(assets);
+
+/*4th req*/
+var profit = 1000;
+var sales = 500;
+var n_income = profit * sales;
+console.log(n_income);
+
+/*5th req*/
+const array = [7, 9, 2];
+var avg = (7+9+2)/3;
+console.log(avg);
+
+/*6th req*/
+var price = 150;
+var dis_ratio = 0.3;
+var discount = price * dis_ratio;
+console.log(discount);
+
+/*7th req*/
+var age = 20;
+if(age > 18 && age < 30) {
+    console.log(true);
+}
+
+/*8th req*/
+var expo = 2 ** 3;
+console.log(expo);
+
+/*9th req*/
+var mod = 2 % 4;
+console.log(mod)
---
+++ JS Tasks/Task2/task2.js
@@ -0,0 +1,74 @@
+// //1st exercise
+// var s = "";
+// for (var i = 1; i <= 10; i++) {
+//     s = s + i;
+// }
+// console.log("Ex.1 Output:", s,)           //***check
+
+// //2nd exercise
+// while (i <= 10) {
+//     i++;
+//     s = s + i;
+// }
+// console.log("Ex.2 Output: ", s)
+
+// //3rd exercise
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log("Ex.3 output: ", i)
+// }
+
+// //4th exercise
+// var sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum +=i;
+// }
+// console.log("Ex.4 Output: ", sum)
+
+// // 5th exerecise                    ***ckeck if we can solve bwithout if
+// for (let i =0; i<= 10; i++) {
+//     if (i % 2 != 0) {
+//         console.log("Ex.5 Output: ", i)
+//     }
+// }
+
+// //6th exercise
+// var age = prompt("Please enter your age:")
+// if (age > 16) {
+//     console.log("Ex.6 Output: ", "You are an adult")
+// }
+
+// //7th exercise
+// var score =prompt("What sore value did you get?")
+// if(score <= 50) {
+//     console.log("You failed the test")
+// }
+
+// //8th exercise
+// var Name = prompt("What is your name?")
+// console.log("Hello, ", Name)
+
+// //9th exercise                                   ***Check - not working
+// var day = [Friday, Saturday, Sunday, Monday, Tuseday, Wednsday, Thersday]
+// for (i=day; i++) {
+//     if (day = Friday && day = Saturday) {
+//         console.log(day, "It's a weekda")
+//     }
+// }
+
+//10th exercise
+var num = prompt("Please enter a number...")
+if (num % 2 == 0) {
+    console.log("The number is even")
+}
+
+//11th exercise
+
+
+
+
+/*-----------------------------------------------------*/
+// var c = 0;
+// for (i = 477.75; i>0; i-=0.25 ) {
+//     c++;
+// }
+// console.log(i)
