41ae65f4d266d8e1b6e9b14a8a9c5d0e03944a9d
rgd97 <rgd.alshobaki@gmail.com>
2024-06-07 18:09:20 +0300

    T4 Added

---
+++ JS Tasks/Task4/task4.js
@@ -0,0 +1,26 @@
+// // //1st required
+// document.getElementById("header").innerHTML = "Welcome to DOM Manipulation!";
+
+// // //2nd req.
+// document.getElementsByClassName("item").innerHTML = "Class item";
+
+// // //3rd req.
+// document.getElementsByTagName("p").innerHTML = "Paragraph Elemen";
+
+// // //4th req.
+
+// let x =document.getElementById("title");
+
+
+button.addEventListener("click", myFunction);
+function myFunction() {
+    document.getElementById("test").innerHTML = "Hello"
+}
+
+
+
+
+
+
+
+
