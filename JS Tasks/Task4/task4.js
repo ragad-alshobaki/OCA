af9a6b321cf0829e714d6c84dd67f14dc2803c41
rgd97 <rgd.alshobaki@gmail.com>
2024-06-11 00:49:35 +0300

    check T4

--- JS Tasks/Task4/task4.js
+++ JS Tasks/Task4/task4.js
@@ -1,23 +1,29 @@
 //1st required
-var x = "Welcome to DOM Manipulation!";
-document.getElementById("header").innerHTML = x;
+document.getElementById("header").innerHTML = "Welcome to DOM Manipulation!";
 
 //2nd req.
-document.getElementsByClassName("item").innerHTML = "Class item";
+let myClass = document.getElementsByClassName("item");
+console.log(myClass);
+// document.getElementsByClassName("item").innerHTML = "Class Item";    ** check didn't change the txt
+myClass[0].innerHTML = "Class Item";
 
 //3rd req.
-document.getElementsByTagName("p").innerHTML = "Paragraph Elemen";
+let myTag = document.getElementsByTagName("p");
+console.log(myTag);
+myTag[1].innerHTML = "Paragraph Element";
 
 //4th req.
-let x =document.getElementById("title");
+let myQuery = document.querySelector(".highlight");
+console.log(myQuery);
+myQuery.innerHTML = "First Highlighted Item";
 
+let myQueryAll = document.querySelectorAll(".highlight");
+console.log(myQueryAll);
 
-button.addEventListener("click", myFunction);
-function myFunction() {
-    document.getElementById("test").innerHTML = "Hello"
-}
-
-
+//5th req.
+let selectAll = document.querySelectorAll("li");
+console.log(selectAll);
+selectAll.innerHTML = "List Item";
 
 
 
