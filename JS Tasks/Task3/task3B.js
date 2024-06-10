bba8fa68aaac91cf5ee0f803cc92de200b197a2c
rgd97 <rgd.alshobaki@gmail.com>
2024-06-10 23:17:22 +0300

    add

--- JS Tasks/Task3/task3B.js
+++ JS Tasks/Task3/task3B.js
@@ -1,22 +1,27 @@
 //1st exercise
 const pi = 3.14;
 function calculateArea(radius) {
-    return area = pi * radius *2;
+    return area = pi * radius ** 2;
 }
 console.log(calculateArea(5));
 console.log(calculateArea(10));
 
 // //2nd exercise
-function reversestring(str) {
+function reverseString(str) {
     return str.split("").reverse().join("");
 }
-console.log(reversestring('hello'))
-console.log(reversestring('world'))
+console.log(reverseString('hello'))
+console.log(reverseString('world'))
 
 //3rd exercise
-// let arr = [1, 2, 3, 4, 5, 6];
-// let arr2 = [11, 22, 33, 44, 55]
-const filterEvenNumbers = function(arr) {
+// const filterEvenNumbers = (arr) => {
+//     for (i in arr) {
+//         if ( arr[i] % 2 == 0 ) {
+//             return arr;
+//         }
+//     }
+// }
+const filterEvenNumbers = (arr) => {
     let even = [];
     for (i in arr) {
         if ( arr[i] % 2 == 0 ) {
@@ -27,8 +32,8 @@ const filterEvenNumbers = function(arr) {
 }
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 console.log(filterEvenNumbers([11, 22, 33, 44, 55]));
-// filterEvenNumbers(arr);
-// : ? 
+
+// : ?  ternary operator
 
 //4th exercise
 // const library {
@@ -36,3 +41,14 @@ console.log(filterEvenNumbers([11, 22, 33, 44, 55]));
 //     title : 
 //     author :
 // }
+
+//5th exercise
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function numSum() {
+//     var sum = 0;
+//     for (i in numbers) {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// console.log(numSum(sum));
