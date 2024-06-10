192a4f38426b22803dd4005604661032c27442da
rgd97 <rgd.alshobaki@gmail.com>
2024-06-10 18:47:09 +0300

    upd.

--- JS Tasks/Task2/task2.js
+++ JS Tasks/Task2/task2.js
@@ -1,61 +1,61 @@
-// //1st exercise
-// var s = "";
-// for (var i = 1; i <= 10; i++) {
-//     s = s + i;
-// }
-// console.log("Ex.1 Output:", s,)           //***check
+//1st exercise
+var s = "";
+for (var i = 1; i <= 10; i++) {
+    s = s + i;
+}
+console.log("Ex.1 Output:", s,)           //***check
 
-// //2nd exercise
-// while (i <= 10) {
-//     i++;
-//     s = s + i;
-// }
-// console.log("Ex.2 Output: ", s)
+//2nd exercise
+while (i <= 10) {
+    i++;
+    s = s + i;
+}
+console.log("Ex.2 Output: ", s)
 
-// //3rd exercise
-// for (let i = 0; i <= 10; i += 2) {
-//     console.log("Ex.3 output: ", i)
-// }
+//3rd exercise
+for (let i = 0; i <= 10; i += 2) {
+    console.log("Ex.3 output: ", i)
+}
 
-// //4th exercise
-// var sum = 0;
-// for (let i = 1; i <= 10; i++) {
-//     sum +=i;
-// }
-// console.log("Ex.4 Output: ", sum)
+//4th exercise
+var sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum +=i;
+}
+console.log("Ex.4 Output: ", sum)
 
-// // 5th exerecise                    ***ckeck if we can solve bwithout if
-// for (let i =0; i<= 10; i++) {
-//     if (i % 2 != 0) {
-//         console.log("Ex.5 Output: ", i)
-//     }
-// }
+// 5th exerecise                    ***ckeck if we can solve without if
+for (let i =0; i<= 10; i++) {
+    if (i % 2 != 0) {
+        console.log("Ex.5 Output: ", i)
+    }
+}
 
-// //6th exercise
-// var age = prompt("Please enter your age:")
-// if (age > 16) {
-//     console.log("Ex.6 Output: ", "You are an adult")
-// }
+//6th exercise
+var age = prompt("Please enter your age:")
+if (age > 16) {
+    console.log("Ex.6 Output: ", "You are an adult")
+}
 
-// //7th exercise
-// var score =prompt("What sore value did you get?")
-// if(score <= 50) {
-//     console.log("You failed the test")
-// }
+//7th exercise
+var score =prompt("What sore value did you get?")
+if(score <= 50) {
+    console.log("You failed the test")
+}
 
-// //8th exercise
-// var Name = prompt("What is your name?")
-// console.log("Hello, ", Name)
+//8th exercise
+var Name = prompt("What is your name?")
+console.log("Hello, ", Name)
 
-// //9th exercise                                   ***Check - not working
+//9th exercise                                   ***Check - not working
 // var day = [Friday, Saturday, Sunday, Monday, Tuseday, Wednsday, Thersday]
-// for (i=day; i++) {
+// for (let i in day; i++) {
 //     if (day = Friday && day = Saturday) {
-//         console.log(day, "It's a weekda")
+//         console.log(day, "It's a weekend")
 //     }
 // }
 
-//10th exercise
+// 10th exercise
 var num = prompt("Please enter a number...")
 if (num % 2 == 0) {
     console.log("The number is even")
@@ -66,9 +66,18 @@ if (num % 2 == 0) {
 
 
 
-/*-----------------------------------------------------*/
+/*---------------------------------------------------------*/
+//Coach salameh exercise:
 // var c = 0;
 // for (i = 477.75; i>0; i-=0.25 ) {
 //     c++;
 // }
 // console.log(i)
+//with func.
+// var c = 0;
+// function counter (start, end) {
+//     for (let i = start; i > end; i-=0.25) {
+//         c++; }
+// }
+// counter(477.75, 0);
+// console.log(c);
--- JS Tasks/Task4/task4.js
+++ JS Tasks/Task4/task4.js
@@ -1,15 +1,15 @@
-// // //1st required
-// document.getElementById("header").innerHTML = "Welcome to DOM Manipulation!";
+//1st required
+var x = "Welcome to DOM Manipulation!";
+document.getElementById("header").innerHTML = x;
 
-// // //2nd req.
-// document.getElementsByClassName("item").innerHTML = "Class item";
+//2nd req.
+document.getElementsByClassName("item").innerHTML = "Class item";
 
-// // //3rd req.
-// document.getElementsByTagName("p").innerHTML = "Paragraph Elemen";
+//3rd req.
+document.getElementsByTagName("p").innerHTML = "Paragraph Elemen";
 
-// // //4th req.
-
-// let x =document.getElementById("title");
+//4th req.
+let x =document.getElementById("title");
 
 
 button.addEventListener("click", myFunction);
