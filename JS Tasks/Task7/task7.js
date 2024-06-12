c8d01b37886d7a0a4ab220bfbf8b6b6c79fc8a78
rgd97 <rgd.alshobaki@gmail.com>
2024-06-12 11:01:29 +0300

    Update

--- JS Tasks/Task7/task7.js
+++ JS Tasks/Task7/task7.js
@@ -10,15 +10,14 @@ console.log("Task.2 Output:", filterNums);
 
 //Task#3
 // let str = "Orange Coding Academy";
-let strArr = ["ragd", "yousef", "alshobaki"]
 // let getUpChar = str.split("").forEach((i) => i === i.toUpperCase() ? i.toUpperCase() : i.toLowerCase()).join("");
-let getUpChar = strArr.forEach(function(i) {console.log(i.toUpperCase())} );
-
 // let getUpChar = str.split("").forEach(function(i) {
 //   i === i.toUpperCase() ? i.toUpperCase() : i.toLowerCase()
 //   return i} ).join("");
+let strArr = ["ragad", "yousef", "alshobaki"]
+let getUpChar = strArr.forEach(function(i) {console.log(i.toUpperCase())} );
 
-console.log("Task.3 Output:", getUpChar);
+// console.log("Task.3 Output:", getUpChar);
 
 //Task#4
 let device = [{Brand : "HP",
@@ -38,4 +37,28 @@ let fltrProp = device.filter((i) => i.Brand == "Dell")
 console.log("Task.5 Output:", fltrProp);
 
 //Task#6
-let nArr = [5, 10, 15, 20, 25]
+let sumNum = [5, 10, 15, 20, 25]
+let sum = sumNum.forEach((cum, i) => cum+i)
+console.log("Task.6 Output:", sum);
+
+//Task#7
+// let str = "Orange Coding Academy";
+let revArr = ["egnarO", "gnidoC", "ymedacA"]
+let rev = revArr.map((i) => i.split("").reverse().join(""))
+console.log("Task.7 Output:", rev);
+
+//Task#8
+let n = [1, 2, 3, 4, 5, 6, 7]
+let getPrime = n.filter(i => i % 2 === 1)
+console.log("Task.8 Output:", getPrime);
+
+//Task#9
+
+
+//Task#10
+let getStr = n.map(i => i.toString())
+console.log("Task.10 Output:", getStr);
+
+//Task#11
+let dub = [1, 1, 2, 3, 3, 4, 4]
+let getUnique = dub.filter()
