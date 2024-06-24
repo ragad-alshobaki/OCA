c33e57a44bbf8060c1154e367ff3e8235a5f4120
rgd97 <rgd.alshobaki@gmail.com>
2024-06-24 06:36:29 +0300

    T8 Added

---
+++ JS Tasks/Task8/task8.js
@@ -0,0 +1,14 @@
+// 1.
+class Car {
+    make;
+    model;
+    constructor(Make, Model) {
+        this.make = Make;
+        this.model = Model;
+    }
+}
+
+let myCar = new Car ("Mercedes", "G-class");
+console.log(myCar);
+
+// 2.
