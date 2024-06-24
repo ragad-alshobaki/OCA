40366f1c01fb819c3f01f9b1f9d7db7bc81b0354
rgd97 <rgd.alshobaki@gmail.com>
2024-06-24 07:23:23 +0300

    Add

--- JS Tasks/Task8/task8.js
+++ JS Tasks/Task8/task8.js
@@ -6,9 +6,32 @@ class Car {
         this.make = Make;
         this.model = Model;
     }
+    start() {                                   //2
+        console.log("The car has started");
+    }
 }
-
 let myCar = new Car ("Mercedes", "G-class");
 console.log(myCar);
 
 // 2.
+myCar.start()
+
+//4
+class ElectricCar extends Car {
+    battery_size;
+}
+
+//5
+class Account {
+    #balance;
+    deposit() {
+        console.log("You deposit to your balance successfully");
+    }
+    withdraw() {
+        console.log("Your balance has been withdrawn");
+    }
+    set setBalance (blnc) {
+        
+    }
+}
+let x = new Account()
