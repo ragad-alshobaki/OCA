17a265aac02a4666d52cf4634629b968c1998b97
rgd97 <rgd.alshobaki@gmail.com>
2024-06-26 10:38:31 +0300

    Add

--- JS Tasks/Task9/task9.js
+++ JS Tasks/Task9/task9.js
@@ -1,6 +1,48 @@
-let file = "https://jsonplaceholder.typicode.com/posts"
-fetch (file)
-.then(x => x.text())
-.then(y => document.getElementById("showData").innerHTML = y);
+//1: Fetch and Display Posts
+
+// let x = fetch("https://jsonplaceholder.typicode.com/posts")
+// let response = x.then((response)=>{
+//     return response.json()
+// })
+// .then()
+
+let show = document.getElementById("showData1")
+const file = "https://jsonplaceholder.typicode.com/posts"
+fetch (file).then(show => show.json())
+.then(data => {
+    let arrange=""
+    data.forEach(element => {
+        arrange += `<h3>The Title is : ${element.title} </h3>`
+        arrange += `<p>The Body is : ${element.body} </p>`
+    });
+    show.innerHTML=arrange
+});
 // .catch(error => {
 //     console.error('Error fetching posts:', error);});
+
+
+//2: Fetch and Display Comments for a Post
+let show2 = document.getElementById("showData2")
+const file2 = "https://jsonplaceholder.typicode.com/comments?postId=1"
+fetch (file2).then(show2 => show2.json())
+.then(dta => {
+    let arr=""
+    dta.forEach(element => {
+        arr += `<h4>Name: ${element.name}</h4>`
+        arr += `<p>Email Id: <u>${element.email}</u></p>`
+        arr += `<p>The Body is: ${element.body}</p>`
+    });
+    show2.innerHTML=arr;
+});
+
+//3: Fetch and Display User Albums
+let show3 = document.getElementById("showData3")
+const file3 = "https://jsonplaceholder.typicode.com/albums?userId=1"
+fetch(file3).then(show3 => show3.json())
+.then(userData => {
+    let arng = ""
+    userData.forEach(element => {
+        arng += `<h4>User Album: ${element.title}</h4>`
+    });
+    show3.innerHTML = arng;
+})
