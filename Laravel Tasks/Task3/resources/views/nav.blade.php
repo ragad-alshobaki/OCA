113fe6da9b0f559b6bc9d87f3c01b3960e108fc2
ragad-alshobaki <rgd.alshobaki@gmail.com>
2024-08-21 21:05:03 +0300

    check

--- Laravel Tasks/Task3/resources/views/nav.blade.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar">
-    <ul class="navbar-menu">
-        <li class="navbar-item active"><a href="index">Home</a></li>
-        <li class="navbar-item"><a href="#">Categories</a></li>
-        <li class="navbar-item"><a href="index">Products</a></li>
-    </ul>
-</nav>
